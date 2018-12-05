0488a82d1d8c8577182ced6a503516c44a69107c
HouJian <hj1254123@qq.com>
2018-12-05 16:17:28 +0800

    导入css文件，需要安装style-loader css-loader，并配置webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 // 这是ES6的语法，导入了lodash，并赋值给了 _
 import _ from 'lodash'
+// 这里引入css文件必须安装：npm install --save-dev style-loader css-loader
+// 并配置webpack.config.js
+import './style.css'
 
 function componetn() {
     var element = document.createElement('div')
     // 这里就可以使用了
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.classList.add('hello')
     return element
 }
 document.body.appendChild(componetn())
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,18 @@ module.exports = {
         filename: 'main.js',
         // 输出位置
         path: path.resolve(__dirname, 'dist')
+    },
+    // 管理模块
+    module: {
+        // 指定规则
+        rules: [{
+            // 这里是匹配所有以 .css 结尾的文件
+            test: /\.css$/,
+            // use使用，以.css结尾的文件都会被提供给这两个loader
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
     }
 }
